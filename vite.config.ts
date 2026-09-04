5ee42ea674789909cb3d142056394537e23e7822
KalistusAlvino <kalistus33@gmail.com>
2026-09-05 01:28:36 +0700

    ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
-    devtools(),
     tanstackStart(),
+    nitro(),
     viteReact()
   ],
 })
